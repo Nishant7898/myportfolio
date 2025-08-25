6ee4dc2da07b578c10922dfcb023ac6989ff9486
nishant7898 <nishantpathak271@gmail.com>
2025-08-26 04:48:08 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import PageA from "./Pages/PageA";
 import PageB from "./Pages/PageB";
 import PageC from "./Pages/pageC";
-import Header from "./Components/Header";
+
+import Button from "./Components/Header";
 
 const App = () => {
   return (
     <>
-      <Header />
+      <Button />
       <PageA />
       <PageB />
       <PageC />
--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { CiMenuKebab } from "react-icons/ci";
 
-const Header = () => {
+const Button = () => {
   return (
     <div className="w-full fixed justify-end flex items-center p-20 z-10">
       <button className="bg-black gap-2 flex text-center items-center border-4 rounded-full text-xl hover:bg-gray-600    text-white py-4 px-4">
@@ -12,4 +12,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default Button;
