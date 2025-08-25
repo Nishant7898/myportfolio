6979afd82bd8e27d17764477360655de4b7dfdf3
nishant7898 <nishantpathak271@gmail.com>
2025-08-26 04:50:03 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import PageA from "./Pages/PageA";
 import PageB from "./Pages/PageB";
 import PageC from "./Pages/pageC";
 
-import Button from "./Components/Header";
+import Hire from "./Components/Hire";
 
 const App = () => {
   return (
     <>
-      <Button />
+      <Hire />
       <PageA />
       <PageB />
       <PageC />
