6630c454da399a9bb110bfb84ed87a413b9153de
nishant7898 <nishantpathak271@gmail.com>
2025-08-26 03:40:05 +0530

    made responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Page1 from "./Pages/page1";
 
 import Page2 from "./Pages/Page2";
 import Page3 from "./Pages/Page3";
-import Header from "./Components/header";
+import Header from "./Components/Header";
  
 const App = () => {
   return (
