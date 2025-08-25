9ae25c5ea6e34099de74b286fa8bf9b596f837d5
nishant7898 <nishantpathak271@gmail.com>
2025-08-26 03:27:18 +0530

    made responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Page1 from "./Pages/page1";
-import Header from "./Components/Header";
+
 import Page2 from "./Pages/page2";
 import Page3 from "./Pages/Page3";
+import Header from "./Components/header";
  
 const App = () => {
   return (
     <>
-      <Header />
+    <Header/>
       <Page1 />
       <Page2 />
       <Page3 />
